e1b981f4bc91ba5e253ebadb16f25d9f4b7bed57
anhf0309 <anhf0309@gmail.com>
2024-02-20 11:51:08 -0800

    first commit

---
+++ src/main/java/org/example/myservletdemo1/HelloServlet.java
@@ -0,0 +1,51 @@
+package org.example.myservletdemo1;
+
+import java.io.*;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        String user = "user";
+        String userPassword = "123";
+        String username = request.getParameter("myName");
+        String password = request.getParameter("password");
+        if(user.equals(username) & password.equals(userPassword)) {
+            response.setContentType("text/html");
+            PrintWriter pw = response.getWriter();
+            request.setAttribute("username", username);
+            request.setAttribute("password", password);
+            RequestDispatcher rd = request.getRequestDispatcher("/success.jsp");
+            rd.forward(request, response);
+        }
+        else {
+            request.setAttribute("username", username);
+            request.setAttribute("password", password);
+            RequestDispatcher rd = request.getRequestDispatcher("/fail.jsp");
+            rd.forward(request, response);
+        }
+    }
+    public void destroy() {
+    }
+}
